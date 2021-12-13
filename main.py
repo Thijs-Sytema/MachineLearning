bb5aa7fbf2a1be922ff47d5e19167b0e0e8bc8f4
ThijsSytema <57255526+Thijs-Sytema@users.noreply.github.com>
2021-12-13 13:55:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from _ml import MLAgent, train, save, load, train_and_plot
+from _core import is_winner, opponent, start
+ 
+start()
+
+
+ class MyAgent(MLAgent):
+     def evaluate(self, board):
+         if is_winner(board, self.symbol):
+             reward = 1
+         elif is_winner(board, opponent[self.symbol]):
+             reward = -1
+         else:
+             reward = 0
+         return reward
+    
+ 
+ my_agent = MyAgent()
+ my_agent = load('MyAgent_3000')
+
+ my_agent.learning = False
+
+ start(player_x=my_agent)
+
+
