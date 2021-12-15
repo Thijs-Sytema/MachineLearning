3c57cd72ddaa39313e3287ccdec6b1c0e29b3aa4
Thijs Sytema <57255526+Thijs-Sytema@users.noreply.github.com>
2021-12-15 10:51:01 +0000

    grafiek toegevoegd (2.2 fundament)

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
-from _ml import MLAgent, train, save, load, train_and_plot
+from _ml import MLAgent, train, save, load, train_and_plot, validate, plot_validation
 from _core import is_winner, opponent, start
+from _agent import RandomAgent
  
-start()
 
-
- class MyAgent(MLAgent):
-     def evaluate(self, board):
-         if is_winner(board, self.symbol):
-             reward = 1
-         elif is_winner(board, opponent[self.symbol]):
-             reward = -1
-         else:
-             reward = 0
-         return reward
+ 
+ 
+class MyAgent(MLAgent):
+    def evaluate(self, board):
+        if is_winner(board, self.symbol):
+            reward = 1
+        elif is_winner(board, opponent[self.symbol]):
+            reward = -1
+        else:
+            reward = 0
+        return reward
     
+	 
+	 
+my_agent = load('MyAgent_3000')
+my_agent.learning = False
  
- my_agent = MyAgent()
- my_agent = load('MyAgent_3000')
-
- my_agent.learning = False
-
- start(player_x=my_agent)
-
-
+validation_agent = RandomAgent()
+ 
+validation_result = validate(agent_x=my_agent, agent_o=validation_agent, iterations=100)
+ 
+plot_validation(validation_result)
