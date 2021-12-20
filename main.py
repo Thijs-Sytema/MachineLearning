4bee99dbf0bbaef65d8ecaaf4d1f40edb412ad4f
Thijs Sytema <57255526+Thijs-Sytema@users.noreply.github.com>
2021-12-20 14:05:56 +0000

    lijngrafiek toegevoegd, agent kan nu trainen

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 from _ml import MLAgent, train, save, load, train_and_plot, validate, plot_validation
 from _core import is_winner, opponent, start
 from _agent import RandomAgent
+import random
 
 class MyAgent(MLAgent):
-  def evaluate(self, board):
-      if is_winner(board, self.symbol):
-          reward = 1
-      elif is_winner(board, opponent[self.symbol]):
-          reward = -1
-      else:
-          reward = 0
-      return reward
+    def evaluate(self, board):
+        if is_winner(board, self.symbol):
+            reward = 1
+        elif is_winner(board, opponent[self.symbol]):
+            reward = -1
+        else:
+            reward = 0
+        return reward
     
+    
+random.seed(1)
  
-my_agent = load('MyAgent_3000')
-my_agent.learning = False
- 
-validation_agent = RandomAgent()
- 
-validation_result = validate(agent_x=my_agent, agent_o=validation_agent, iterations=100)
- 
-plot_validation(validation_result)
+my_agent = MyAgent()
+random_agent = RandomAgent()
+
+train_and_plot(
+    agent=my_agent,
+    validation_agent=random_agent,
+    iterations=50,
+    trainings=1000,
+    validations=1000)
